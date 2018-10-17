8970f1b43d3f694113dd9bcf0fca6e9bbcd63ef5
Troy Larson <troylar@gmail.com>
2018-10-17 08:13:42 -0400

    Converted to table

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import os.path
 import time
 from dateutil.parser import parse
 import re
+import pandas as pd
 
 ignore_subjects = [u'WordBuild Elements 1',
                    u'Personal and Family Finance',
@@ -37,17 +38,22 @@ week_d = []
 total_plus_b = 0
 total_units_this_week = 0
 total_units_today = 0
+data_frame = {}
+
 for subject in subjects:
     s = data[subject]
+    data_frame[subject] = []
     done[subject] = {}
     done[subject]['week'] = 0
     done[subject]['day'] = 0
     assignments = s['assignments']
+    data_frame[subject].append(subject)
     try:
-       grade = int(data[subject]['grade'])
-       if grade >= 80:
-          total_plus_b = total_plus_b + 1
-       grades.append(grade)
+        grade = int(data[subject]['grade'])
+        if grade >= 80:
+            total_plus_b = total_plus_b + 1
+        grades.append(grade)
+        data_frame[subject].append(grade)
     except ValueError:
         grades.append(0)
 
@@ -56,12 +62,14 @@ for subject in subjects:
         completion.append(percent)
     except ValueError:
         completion.append(0)
+        data_frame[subject].append(0)
     latest_date = parse('1/1/2000')
     latest_unit = 0
     highest_unit = 0
     total_units = 0
     completed_units = {}
     s['units_this_week'] = 0
+    s['units_today'] = 0
     unit_done_dates = {}
     for a in assignments:
         m = re.match('^([0-9]+)\.([0-9]+)', a)
@@ -102,14 +110,40 @@ for subject in subjects:
             s['latest_unit'] = unit
             if unit_done_dates[unit] == today:
                 total_units_today = total_units_today + 1
+                s['units_today']= s['units_today'] + 1
             if unit_done_dates[unit] < end_week and unit_done_dates[unit] > start_week:
                 s['units_this_week'] = s['units_this_week'] + 1
                 total_units_this_week = total_units_this_week + 1
     s['completion_date'] = arrow.now().shift(weeks=+(highest_unit - int(s['latest_unit'])))
-
-
+    data_frame[subject].append(arrow.get(latest_date).shift(hours=+1).humanize())
+    data_frame[subject].append('{} of {} ({})'.format(s['latest_unit'], s['highest_unit'], s['percentage']))
+    data_frame[subject].append(done[subject]['day'])
+    data_frame[subject].append(done[subject]['week'])
+    data_frame[subject].append(s['units_today'])
+    data_frame[subject].append(s['units_this_week'])
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+def generate_table(data, max_rows=100):
+    dataframe = pd.DataFrame.from_dict(data, orient='index',
+				       columns=['Subject',
+                                                'Grade',
+                                                'Latest Activity',
+                                                'Highest Unit',
+                                                'Items Done Today',
+                                                'Items Done This Week',
+                                                'Units Done Today',
+                                                'Units Done This Week',
+                                               ])
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
 
 
 def generate_subject_widget(subject):
@@ -128,16 +162,19 @@ def generate_subject_widget(subject):
             html.H5(
                 'Current Grade: {}'.format(s['grade'] + '%')),
             html.H5(
-                'Done Today: {}'.format(done[subject]['day'])
+                'Assignments Done:'
+            ),
+            html.H6(
+                'Today/This Week: {}/{}'.format(done[subject]['day'], done[subject]['week'])
             ),
             html.H5(
-                'Done This Week: {}'.format(done[subject]['week'])
+                'Units Done:'
             ),
-            html.H5(
+            html.H6(
+                'Today/This Week: {}/{}'.format(s['units_today'], s['units_this_week'])
+            ),
+             html.H5(
                 'Latest Unit: {} of {}'.format(s['latest_unit'], s['highest_unit'])
-            ),
-            html.H5(
-                'Units This Week: {}'.format(s['units_this_week'])
             ),
             html.H5(
                 'Est Completion: {}'.format(s['completion_date'].format('MMM D, YYYY'))
@@ -156,14 +193,14 @@ def render_layout():
     return html.Div(children=
 	     [
 		 html.Div(children=[
-		     html.H1("Chloe's School Year 2018-2019"),
-		     html.H4('Last Updated: {}'.format(arrow.get(last_modified).humanize())),
-		     html.H3('Week: {} - {}'.format(arrow.get(start_week).format('ddd, MMM D'), arrow.get(end_week).format('ddd, MMM D'))),
-		     html.H3('Total Units Done Today: {}'.format(total_units_today)),
-		     html.H3('Total Units Done This Week: {} of 7'.format(total_units_this_week)),
-		     html.H3('Required B\'s: {} of {}'.format(total_plus_b, len(subjects)))
+		     html.H4("Chloe's School Year 2018-2019"),
+		     html.H6('Last Updated: {}'.format(arrow.get(last_modified).humanize())),
+		     html.H5('Week: {} - {}'.format(arrow.get(start_week).format('ddd, MMM D'), arrow.get(end_week).format('ddd, MMM D'))),
+		     html.H5('Total Units Done Today: {}'.format(total_units_today)),
+		     html.H5('Total Units Done This Week: {} of 7'.format(total_units_this_week)),
+		     html.H5('Required B\'s: {} of {}'.format(total_plus_b, len(subjects)))
 		 ]),
-		 html.Div(children=grade_widgets())
+		 html.Div(children=generate_table(data_frame))
 	     ],
 	     className="row"
 	)
